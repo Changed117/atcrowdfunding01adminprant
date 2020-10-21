a84961f911a4d1a8e251ef01bb5e2ad229649e7c
Changed <1376758306@qq.com>
2020-10-21 15:39:13 +0800

    2020.10.21

---
+++ src/main/java/com/Changed/crowd/exception/AccessForbiddenException.java
@@ -0,0 +1,20 @@
+package com.Changed.crowd.exception;
+
+public class AccessForbiddenException extends RuntimeException{
+
+    public AccessForbiddenException() {
+    }
+
+    public AccessForbiddenException(String message) {
+        super(message);
+    }
+
+    public AccessForbiddenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AccessForbiddenException(Throwable cause) {
+        super(cause);
+    }
+
+}
