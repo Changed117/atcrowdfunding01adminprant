f9910c97c2d4e738c57a754e6fa8b96edd108ae3
Changed <1376758306@qq.com>
2020-10-24 22:48:13 +0800

    2020.10.24

---
+++ src/main/java/com/Changed/crowd/exception/LoginAcctAlreadyInUseForUpdateException.java
@@ -0,0 +1,22 @@
+package com.Changed.crowd.exception;
+
+public class LoginAcctAlreadyInUseForUpdateException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public LoginAcctAlreadyInUseForUpdateException() {
+    }
+
+    public LoginAcctAlreadyInUseForUpdateException(String message) {
+        super(message);
+    }
+
+    public LoginAcctAlreadyInUseForUpdateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public LoginAcctAlreadyInUseForUpdateException(Throwable cause) {
+        super(cause);
+    }
+
+}
